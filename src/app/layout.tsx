b50340c1ac4dcc014d47574ac27af620e31cbd0c
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-09 19:02:19 -0400

    DELETE: Deleted font inter from app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
+import NavBar from '@/components/navBar/NavBar'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from '../components/navBar/NavBar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'SegurtecS.U.R.L',
@@ -17,9 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     
-     <link rel="icon" href="logo01.png" />
-      <body className={inter.className}>
+
+      <link rel="icon" href="logo01.png" />
+      <body >
         <NavBar />
         {children}
       </body>
