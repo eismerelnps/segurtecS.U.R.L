5117a31d5073d5c84d08950d5c85811b80bb71c9
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-09 23:15:44 -0400

    FIX: Fixed some sizing on AboutMain and ServicesCards

--- src/components/About/AboutMain.tsx
+++ src/components/About/AboutMain.tsx
@@ -4,12 +4,15 @@ import banner_01 from '/public/banner_01.webp'
 
 export default function AboutMain() {
     return (
-        <div className='  bg-white shadow shadow-neutral-50 sm:p-8 lg:p-16'>
+        <div className='bg-white shadow shadow-neutral-50 sm:p-8 lg:p-16'>
             <div className='flex flex-col lg:flex-row justify-center px-4 py-8 sm:px-2 sm:py-4 lg:px-8 lg:py-16'>
                 <div className='basis-full lg:basis-1/2 '>
+                    <div className='block lg:hidden my-8'>
+                        <h1 className='text-2xl lg:text-4xl 2xl:text-6xl text-center text-neutral-950 font-bold my-2 2xl:mt-8 2xl:mb-4 '>¿Quienes somos?</h1>
+                        <p className='text-base lg:text-lg  2xl:text-3xl sm:mx-16  text-center text-neutral-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, modi. Exercitationem dolor inventore ipsa quibusdam culpa odit ab nihil molestiae, nostrum, sint natus sit aliquid, aliquam laudantium eius deserunt deleniti.</p>
+                    </div>
                     <div className='flex items-center h-full'>
                         <Image
-
                             className='rounded-2xl'
                             src={banner_01}
                             alt={'Segurtec banner'}
@@ -31,8 +34,11 @@ export default function AboutMain() {
                 <div className='basis-full lg:basis-1/2 p-2 sm:p-6 lg:p-8  2xl:py-8'>
                     <div className='flex  items-center  h-full'>
                         <div>
-                            <h1 className='text-xl lg:text-4xl 2xl:text-8xl text-center text-neutral-950 font-bold my-2 2xl:mt-8 2xl:mb-4 '>¿Quienes somos?</h1>
-                            <p className='2xl:text-3xl text-center text-neutral-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, modi. Exercitationem dolor inventore ipsa quibusdam culpa odit ab nihil molestiae, nostrum, sint natus sit aliquid, aliquam laudantium eius deserunt deleniti.</p>
+                            <div className='hidden lg:block'>
+                                <h1 className='text-xl lg:text-4xl 2xl:text-6xl text-center text-neutral-950 font-bold my-2 2xl:mt-8 2xl:mb-4 '>¿Quienes somos?</h1>
+                                <p className='2xl:text-3xl text-center text-neutral-800'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, modi. Exercitationem dolor inventore ipsa quibusdam culpa odit ab nihil molestiae, nostrum, sint natus sit aliquid, aliquam laudantium eius deserunt deleniti.</p>
+
+                            </div>
                             <div className='flex flex-row flex-wrap justify-center  my-2 2xl:my-8'>
 
 
--- src/components/Services/ServicesList.tsx
+++ src/components/Services/ServicesList.tsx
@@ -4,16 +4,19 @@ import ServicesCard from './ServicesCard'
 export default function ServicesList() {
     return (
 
-        <section className='flex flex-col sm:flex-row flex-wrap my-8 '>
-            <article className='w-full text-center mt-4 mb-8'>
-                <h2 className='text-4xl'>Nuestros servicios</h2>
-                <p className='my-4 mx-4 sm:mx-8 xl:mx-16'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi labore praesentium officiis, reprehenderit tempora dolorum iure consequatur, earum dignissimos doloribus fuga voluptates libero, accusantium et quaerat itaque sunt eligendi quod.</p>
+        <section className='flex flex-col sm:flex-row flex-wrap sm:p-8 lg:p-16 '>
+            <article className='w-full text-center px-4 py-8 sm:px-2 sm:py-4 lg:px-8 lg:py-16'>
+                <h2 className='text-xl lg:text-4xl 2xl:text-6xl text-neutral-950 font-bold'>Nuestros servicios</h2>
+                <p className='my-4 mx-4 lg:mx-16 xl:mx-24'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi labore praesentium officiis, reprehenderit tempora dolorum iure consequatur, earum dignissimos doloribus fuga voluptates libero, accusantium et quaerat itaque sunt eligendi quod.</p>
             </article>
-            {
-                mainServices.map((service) => (
-                    <ServicesCard key={service.id} service={service} />
-                ))
-            }
+            <article className='flex flex-col sm:flex-row flex-wrap  content-center sm:justify-center'>
+                {
+                    mainServices.map((service) => (
+                        <ServicesCard key={service.id} service={service} />
+                    ))
+                }
+            </article>
+
         </section>
     )
 }
