25785ab834600761f8ccc054d07785cbce00dc84
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-11 18:38:14 -0400

    FEAT: implementinting main banner

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import './promo.css'
+import Image from 'next/image'
+
+import main_portrait from '/public/main_portrait.jpg'
 
 export default function Promo() {
   return (
-    <section className='bg-neutral-200 '>
-       
-
-    
-      <article className='flex flex-col sm:flex-row space-y-8 py-12 px-4 sm:p-16 '>
-        <div className='basis-full sm:basis-1/2 self-center'>
-          <div>
-            <p className='text-md text-red-600 text-center font-bold'>No dejes que la inseguridad te preocupe;</p>
-            <p className='text-sm text-red-600 text-center font-bold'>Déjanos proteer lo que más te importa</p>
+    <section className='relative'>
+      <Image
+        src={main_portrait}
+        alt="portrait Segurtec"
+      />
+      <article className='absolute bg-transparent h-full w-2/3  top-0 left-0 '>
+        <div className=' bg-slate-100/50  flex flex-col justify-center  h-full  lg:mx-12'>
+          <div className=''>
+            <p className=' lg:text-6xl text-white font-bold'>SEGURTEC S.U.R.L</p>
           </div>
+          <div className=' bg-transparent  flex flex-col justify-center  '>
+          <div className=' lg:my-8'>
+        
+            <p className='text-[8px] lg:text-xl text-white font-bold'>No dejes que la inseguridad te preocupe</p>
+            <p className='text-[8px] lg:text-2xl text-white  font-bold'>Déjanos proteer lo que más te importa</p>
+            <p className='text-[8px]'> clasLorem ipsum dolor sit amet consectetur adipisicing elit. Non iusto accusantium corporis doloremque cum perferendis nam dolor, nobis ullam dignissimos error repellat! Nobis, ullam nisi accusantium numquam dolorem vitae recusandae.</p>
 
-        </div>
-        <div className='basis-full sm:basis-1/2'>
-          <div>
-            <p className='text-center'>En Segurtec S.U.R.L </p>
-            <p className='text-center'> clasLorem ipsum dolor sit amet consectetur adipisicing elit. Non iusto accusantium corporis doloremque cum perferendis nam dolor, nobis ullam dignissimos error repellat! Nobis, ullam nisi accusantium numquam dolorem vitae recusandae.</p>
-
+          
           </div>
-
         </div>
+        </div>
+        
+
+
+
+
+
+
       </article>
-     
     </section>
   )
 }
