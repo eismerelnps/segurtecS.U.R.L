f780fe65203fb8efa13f1177a67c1a3c55ba7cca
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-11 18:47:54 -0400

    REFACTORICING the desing

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -10,17 +10,16 @@ export default function Promo() {
         alt="portrait Segurtec"
       />
       <article className='absolute bg-transparent h-full w-2/3  top-0 left-0 '>
-        <div className=' bg-slate-100/50  flex flex-col justify-center  h-full  lg:mx-12'>
-          <div className=''>
-            <p className=' lg:text-6xl text-white font-bold'>SEGURTEC S.U.R.L</p>
+        <div className=' bg-slate-100/50  flex flex-col justify-center  h-full ps-1.5 lg:mx-12'>
+          <div className='my-1 ms-4'>
+            <p className='text-lg lg:text-6xl text-white font-bold '>SEGURTEC S.U.R.L</p>
           </div>
           <div className=' bg-transparent  flex flex-col justify-center  '>
-          <div className=' lg:my-8'>
+          <div className='mx-4 lg:my-8 '>
         
-            <p className='text-[8px] lg:text-xl text-white font-bold'>No dejes que la inseguridad te preocupe</p>
-            <p className='text-[8px] lg:text-2xl text-white  font-bold'>Déjanos proteer lo que más te importa</p>
-            <p className='text-[8px]'> clasLorem ipsum dolor sit amet consectetur adipisicing elit. Non iusto accusantium corporis doloremque cum perferendis nam dolor, nobis ullam dignissimos error repellat! Nobis, ullam nisi accusantium numquam dolorem vitae recusandae.</p>
-
+            <p className='text-[10px] lg:text-xl text-white font-bold'>No dejes que la inseguridad te preocupe</p>
+            <p className='text-[11px] lg:text-2xl text-white  font-bold'>Déjanos proteer lo que más te importa</p>
+           <p className='text-[8px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam aliquid, ullam accusamus maiores enim ipsum quas eaque dolorem maxime ea?</p>
           
           </div>
         </div>
