884815403c731d8852cfcbe814dab5fa89d23b59
Eismer Lobaina <eismerlobaina@gmail.com>
2024-04-15 12:53:30 -0400

    feat: settup intl

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true
-      }
-}
+const withNextIntl = require("next-intl/plugin")("./src/lib/i18n/i18n.ts");
 
-module.exports = nextConfig
+module.exports = withNextIntl({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/de3tluzbk/**",
+      },
+    ],
+  },
+}
+)
