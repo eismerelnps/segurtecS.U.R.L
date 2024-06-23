a663711b1a8c60d7623eea3231a042fa82c2e590
Eismer Lobaina <eismerlobaina@gmail.com>
2024-06-23 02:17:54 -0400

    feat: installed shadnui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //darkMode: "class",
+  // darkMode: ["class"],
   content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
     "./src/**/*.{js,ts,jsx,tsx,ts,mdx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         primary: {
@@ -20,15 +32,25 @@ module.exports = {
           900: "#500000",
           950: "#290000"
         },
-
       },
-      // backgroundImage: {
-      //   "gradient-radial":
-      //     "radial-gradient(circle, rgba(0,0,0,1) 36%, rgba(255,255,255,0) 100%)",
-      //   "gradient-conic":
-      //     "radial-gradient(circle, rgba(0,0,0,1) 36%, rgba(255,255,255,0) 100%)",
-      // },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [
+    require("tailwindcss-animate"),
+    require("daisyui")
+  ],
+}
